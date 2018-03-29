9bdf7d1095f5bccb3a16353b4e5edfa24d772379
zhangjun1 <zhangjun1@corp.netease.com>
2018-03-29 09:28:22 +0800

    new deposit handler

---
+++ src/main/java/com/galaxy/moon/pp/biz/ips/WithdrawHandler.java
@@ -0,0 +1,41 @@
+package com.galaxy.moon.pp.biz.ips;
+
+import com.alibaba.fastjson.JSONObject;
+import com.galaxy.moon.common.Result;
+import com.galaxy.moon.common.ResultGenerator;
+import com.galaxy.moon.pp.common.IPSCONSTANTS;
+import com.galaxy.moon.pp.model.User;
+import com.galaxy.moon.pp.model.dto.WithDrawDTO;
+import com.galaxy.moon.pp.service.UUIDService;
+import com.galaxy.moon.pp.service.UserService;
+import com.galaxy.moon.pp.util.IPSRSAUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * create by zhangjun1 on 2018/3/27
+ * 提现处理类
+ */
+@Component
+public class WithdrawHandler {
+    @Autowired
+    UUIDService uuidService;
+
+    @Autowired
+    UserService userService;
+
+    public Result deposit(JSONObject jsonObject) {
+        String billNo = uuidService.nextStrId();
+        long userId = jsonObject.getLong("userId");
+        User user = userService.findById(userId);
+
+        WithDrawDTO withDrawDTO = new WithDrawDTO();
+        withDrawDTO.setMerBillNo(billNo);
+        withDrawDTO.setMerFee("1");
+
+        String reqStr = JSONObject.toJSONString(withDrawDTO);
+
+        JSONObject result = IPSRSAUtil.genReqData(IPSCONSTANTS.merchantID, "trade.deposit", reqStr);
+        return ResultGenerator.genSuccessResult(result);
+    }
+}
--- src/main/java/com/galaxy/moon/pp/model/dto/WithDrawDTO.java
+++ src/main/java/com/galaxy/moon/pp/model/dto/WithDrawDTO.java
@@ -1,50 +1,98 @@
 package com.galaxy.moon.pp.model.dto;
 
 
-import com.galaxy.moon.pp.util.Validate;
+public class WithDrawDTO {
+    public String merFee;
+    public String merBillNo;
+    public String merFeeType;
+    public String webUrl;
+    public String merDate;
+    public String ipsFeeType;
+    public String userType;
+    public String trdAmt;
+    public String ipsAcctNo;
+    public String s2SUrl;
 
-public class WithDrawDTO implements RequestDTO {
+    public WithDrawDTO() {
+    }
 
-	public WithDrawDTO(String merFee, String merBillNo, String merFeeType,
-                       String webUrl, String merDate, String ipsFeeType, String userType,
-                       String trdAmt, String ipsAcctNo, String s2sUrl) {
-		super();
-		this.merFee = merFee;
-		this.merBillNo = merBillNo;
-		this.merFeeType = merFeeType;
-		this.webUrl = webUrl;
-		this.merDate = merDate;
-		this.ipsFeeType = ipsFeeType;
-		this.userType = userType;
-		this.trdAmt = trdAmt;
-		this.ipsAcctNo = ipsAcctNo;
-		s2SUrl = s2sUrl;
-	}
-	public WithDrawDTO(){}
-	
-	public String merFee;
-	public String merBillNo;
-	public String merFeeType;
-	public String webUrl;
-	public String merDate;
-	public String ipsFeeType;
-	public String userType;
-	public String trdAmt;
-	public String ipsAcctNo;
-	public String s2SUrl;
-	@Override
-	public boolean validate(String merchantID) {  //all check
-		boolean result=true;
-		result = result && Validate.NotEmpty("merBillNo", merBillNo);
-		result = result && Validate.NotEmpty("merDate", merDate);
-		result = result && Validate.oneOrTwo("userType", userType);
-		result = result && Validate.NotEmpty("trdAmt", trdAmt);
-		result = result && Validate.NotEmpty("merFee", merFee);
-		result = result && Validate.oneOrTwo("merFeeType", merFeeType);
-		result = result && Validate.oneOrTwo("ipsFeeType", ipsFeeType);
-		result = result && Validate.NotEmpty("webUrl", webUrl);
-		result = result && Validate.NotEmpty("s2SUrl", s2SUrl);
-		result = result && Validate.NotEmpty("ipsAcctNo", ipsAcctNo);
-		return result;
-	}
+    public String getMerFee() {
+        return merFee;
+    }
+
+    public void setMerFee(String merFee) {
+        this.merFee = merFee;
+    }
+
+    public String getMerBillNo() {
+        return merBillNo;
+    }
+
+    public void setMerBillNo(String merBillNo) {
+        this.merBillNo = merBillNo;
+    }
+
+    public String getMerFeeType() {
+        return merFeeType;
+    }
+
+    public void setMerFeeType(String merFeeType) {
+        this.merFeeType = merFeeType;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    public String getMerDate() {
+        return merDate;
+    }
+
+    public void setMerDate(String merDate) {
+        this.merDate = merDate;
+    }
+
+    public String getIpsFeeType() {
+        return ipsFeeType;
+    }
+
+    public void setIpsFeeType(String ipsFeeType) {
+        this.ipsFeeType = ipsFeeType;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getTrdAmt() {
+        return trdAmt;
+    }
+
+    public void setTrdAmt(String trdAmt) {
+        this.trdAmt = trdAmt;
+    }
+
+    public String getIpsAcctNo() {
+        return ipsAcctNo;
+    }
+
+    public void setIpsAcctNo(String ipsAcctNo) {
+        this.ipsAcctNo = ipsAcctNo;
+    }
+
+    public String getS2SUrl() {
+        return s2SUrl;
+    }
+
+    public void setS2SUrl(String s2SUrl) {
+        this.s2SUrl = s2SUrl;
+    }
 }
