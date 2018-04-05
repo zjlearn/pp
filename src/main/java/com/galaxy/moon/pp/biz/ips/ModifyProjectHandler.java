d74d410f9142d879f2ef81359890c9597641d995
zjlearn <zj_learn@163.com>
2018-04-05 09:28:26 +0800

    添加冻结接口

--- src/main/java/com/galaxy/moon/pp/biz/ips/ModifyProjectHandler.java
+++ src/main/java/com/galaxy/moon/pp/biz/ips/ModifyProjectHandler.java
@@ -1,13 +1,43 @@
 package com.galaxy.moon.pp.biz.ips;
 
+import com.alibaba.fastjson.JSONObject;
 import com.galaxy.moon.common.Result;
 import com.galaxy.moon.common.ResultGenerator;
+import com.galaxy.moon.common.util.DateUtil;
+import com.galaxy.moon.pp.common.IPSCONSTANTS;
+import com.galaxy.moon.pp.model.dto.ModifyProjectDTO;
+import com.galaxy.moon.pp.service.BillIdService;
+import com.galaxy.moon.pp.util.IPSHttpUtil;
+import com.galaxy.moon.pp.util.IPSRSAUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * 借口最后写
+ */
 @Component
 public class ModifyProjectHandler {
+    @Autowired
+    BillIdService billIdService;
+
+    @Autowired
+    IPSHttpUtil ipsHttpUtil;
 
     public Result modify(){
+        ModifyProjectDTO modifyProjectDTO = new ModifyProjectDTO();
+        String billNo = billIdService.nextStrId();
+        modifyProjectDTO.setMerBillNo(billNo);
+        modifyProjectDTO.setMerDate(DateUtil.parseLongToString(System.currentTimeMillis(), DateUtil.defaultDatePattern));
+        modifyProjectDTO.setModifyType("1");
+
+
+        String reqStr = JSONObject.toJSONString(modifyProjectDTO);
+
+        JSONObject result = IPSRSAUtil.genReqData(IPSCONSTANTS.merchantID, "project.modifyProject", reqStr);
+
+        // 发送http请求
+//        ipsHttpUtil.post(IPSCONSTANTS.merchantID, );
 
         return ResultGenerator.genSuccessResult();
     }
---
+++ src/main/java/com/galaxy/moon/pp/controller/ips/freezeController.java
@@ -0,0 +1,40 @@
+package com.galaxy.moon.pp.controller.ips;
+
+import com.alibaba.fastjson.JSONObject;
+import com.galaxy.moon.common.Result;
+import com.galaxy.moon.pp.biz.ips.RegProjectHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 冻结接口
+ */
+@Controller
+@RequestMapping("/xhr/ips/freeze")
+public class freezeController {
+
+    @Autowired
+    RegProjectHandler regProjectHandler;
+
+    @ResponseBody
+    @RequestMapping("/sign")
+    public Result sign(@RequestBody JSONObject jsonObject, HttpSession httpSession) {
+        return regProjectHandler.sign(jsonObject, httpSession);
+    }
+
+    @RequestMapping("/inform")
+    public void inform(@RequestParam("resultCode") String resultCode,
+                       @RequestParam("resultMsg") String resultMsg,
+                       @RequestParam("merchantID") String merchantID,
+                       @RequestParam("sign") String sign,
+                       @RequestParam("response") String response, HttpServletResponse httpResponse) {
+        regProjectHandler.inform(resultCode, resultMsg, merchantID, sign, response, httpResponse);
+    }
+}
